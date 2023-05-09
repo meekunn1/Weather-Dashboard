bb0d6eb357a9a506bda88c1324fadb94a5c6540c
Keiji O <meekunn@gmail.com>
2023-05-09 00:47:49 -0700

    landing page button function

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var APIkey = "7b927c53ca7347b508069d9688a6767e"
+var searchInput = document.querySelector("#searchInput");
+var searchBtn = document.querySelector("#searchBtn");
+
+// Initial state
+
+var searchHistory = [];
+var inputValue = "";
+
+//day.js test to convert dt = unix
+var dtData = dayjs.unix(1662292800);
+console.log(dtData.format('MMM D, YYYY'));
+
+
+//submit button
+searchBtn.addEventListener("click", function(event)
+{
+  event.preventDefault();
+  inputValue = searchInput.value.trim();
+  console.log(inputValue);
+
+  if (!inputValue) {
+    console.error("Please input City name.");
+    return;
+  }
+  var queryString = "./search-result.html?q=" + inputValue;
+
+  location.assign(queryString);
+})
---
+++ assets/search-result.js
@@ -0,0 +1,191 @@
+var APIkey = "7b927c53ca7347b508069d9688a6767e"
+var searchInput = document.querySelector("#searchInput");
+var searchBtn = document.querySelector("#searchBtn");
+var searchResult = document.querySelector("#searchResult");
+var day0 = document.querySelector("#day0");
+
+// Initial state
+
+var searchHistory = [];
+var inputValue = "";
+
+//day.js test to convert dt = unix
+var dtData = dayjs.unix(1662292800);
+console.log(dtData.format('MMM D, YYYY'));
+
+
+//submit button
+searchBtn.addEventListener("click", function(event)
+{
+  event.preventDefault();
+  inputValue = searchInput.value.trim();
+  console.log(inputValue);
+
+  if (!inputValue) {
+    console.error("Please input City name.");
+    return;
+  }
+  var queryString = "./search-result.html?q=" + inputValue;
+
+  location.assign(queryString);
+})
+function getInput(){
+  var inputValue = document.location.search.split('=')[1];
+  console.log(inputValue);
+}
+getInput();
+
+
+function generateWeather() {
+  var searchAPI = "api.openweathermap.org/data/2.5/weather?q=" + inputValue + "&appid=" + APIkey;
+  fetch(searchAPI)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+
+      return response.json();
+    })
+    .then(function (locRes) {
+      // write query to page so user knows what they are viewing
+      resultTextEl.textContent = locRes.search.query;
+
+      console.log(locRes);
+
+      if (!locRes.results.length) {
+        console.log("City name not recognized");
+        resultContentEl.innerHTML = '<h3>City name was not recognized</h3>';
+      } else {
+        resultContentEl.textContent = '';
+        for (var i = 0; i < locRes.results.length; i++) {
+          printResults(locRes.results[i]);
+        }
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
+generateWeather()
+
+
+//sample copy from week6 01-27 script.js
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+//sample copy from week6 01-27 display-search.js
+var resultTextEl = document.querySelector('#result-text');
+var resultContentEl = document.querySelector('#result-content');
+var searchFormEl = document.querySelector('#search-form');
+
+function getParams() {
+  // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
+  var searchParamsArr = document.location.search.split('&');
+
+  // Get the query and format values
+  var query = searchParamsArr[0].split('=').pop();
+  var format = searchParamsArr[1].split('=').pop();
+
+  searchApi(query, format);
+}
+
+function printResults(resultObj) {
+  console.log(resultObj);
+
+  // set up `<div>` to hold result content
+  var resultCard = document.createElement('div');
+  resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+
+  var resultBody = document.createElement('div');
+  resultBody.classList.add('card-body');
+  resultCard.append(resultBody);
+
+  var titleEl = document.createElement('h3');
+  titleEl.textContent = resultObj.title;
+
+  var bodyContentEl = document.createElement('p');
+  bodyContentEl.innerHTML =
+    '<strong>Date:</strong> ' + resultObj.date + '<br/>';
+
+  if (resultObj.subject) {
+    bodyContentEl.innerHTML +=
+      '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
+  } else {
+    bodyContentEl.innerHTML +=
+      '<strong>Subjects:</strong> No subject for this entry.';
+  }
+
+  if (resultObj.description) {
+    bodyContentEl.innerHTML +=
+      '<strong>Description:</strong> ' + resultObj.description[0];
+  } else {
+    bodyContentEl.innerHTML +=
+      '<strong>Description:</strong>  No description for this entry.';
+  }
+
+  var linkButtonEl = document.createElement('a');
+  linkButtonEl.textContent = 'Read More';
+  linkButtonEl.setAttribute('href', resultObj.url);
+  linkButtonEl.classList.add('btn', 'btn-dark');
+
+  resultBody.append(titleEl, bodyContentEl, linkButtonEl);
+
+  resultContentEl.append(resultCard);
+}
+
+function searchApi(query, format) {
+  var locQueryUrl = 'https://www.loc.gov/search/?fo=json';
+
+  if (format) {
+    locQueryUrl = 'https://www.loc.gov/' + format + '/?fo=json';
+  }
+
+  locQueryUrl = locQueryUrl + '&q=' + query;
+
+  fetch(locQueryUrl)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+
+      return response.json();
+    })
+    .then(function (locRes) {
+      // write query to page so user knows what they are viewing
+      resultTextEl.textContent = locRes.search.query;
+
+      console.log(locRes);
+
+      if (!locRes.results.length) {
+        console.log('No results found!');
+        resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+      } else {
+        resultContentEl.textContent = '';
+        for (var i = 0; i < locRes.results.length; i++) {
+          printResults(locRes.results[i]);
+        }
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+  var searchInputVal = document.querySelector('#search-input').value;
+  var formatInputVal = document.querySelector('#format-input').value;
+
+  if (!searchInputVal) {
+    console.error('You need a search input value!');
+    return;
+  }
+
+  searchApi(searchInputVal, formatInputVal);
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+getParams();
+
