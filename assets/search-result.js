aea8d09c38dcdccc432a3a2162d180284f484a61
Keiji O <meekunn@gmail.com>
2023-05-12 00:13:41 -0700

    save function working mvp

--- assets/search-result.js
+++ assets/search-result.js
@@ -2,31 +2,52 @@ var APIkey = "7b927c53ca7347b508069d9688a6767e"
 var searchInput = document.querySelector("#searchInput");
 var searchBtn = document.querySelector("#searchBtn");
 var searchResult = document.querySelector("#searchResult");
+var saveBtn = document.querySelector("#saveBtn");
+var searchHistory = document.querySelector("#searchHistory");
 var day0 = document.querySelector("#day0");
 var notFound = document.querySelector("#notFound");
 var day0temp = document.querySelector("#day0temp");
 var day0wind = document.querySelector("#day0wind");
 var day0humidity = document.querySelector("#day0humidity");
-
-//var day0Icon = document.querySelector(".day0Icon");
+var day3 = document.querySelector("#day3");
+var day11 = document.querySelector("#day11");
+var day19 = document.querySelector("#day19");
+var day27 = document.querySelector("#day27");
+var day35 = document.querySelector("#day35");
 
 // Initial state
-
-var searchHistory = [];
+var saveList = [];
 var inputValue = "";
+var currentCity = "";
 day0.textContent = "";
 notFound.textContent = "";
+
+// Get search history from Local storage
+function loadSaved() {
+  var saved = JSON.parse(localStorage.getItem("saved"));
+  console.log(saved);
+  if (saved !== null) {
+    saveList = saved;
+  }}
+
+loadSaved();
+displaySave();
 
 //day.js test to convert dt = unix
 var dtData = dayjs.unix(1662292800);
 console.log(dtData.format('MMM D, YYYY'));
 
 
-//submit button
+//submit button to determine input value and start search
 searchBtn.addEventListener("click", function(event)
 {
   event.preventDefault();
   inputValue = searchInput.value.trim();
+  startSearch()
+})
+
+//relocate html to start cycle with new input
+function startSearch() {
   console.log(inputValue);
 
   if (!inputValue) {
@@ -36,14 +57,64 @@ searchBtn.addEventListener("click", function(event)
   var queryString = "./search-result.html?q=" + inputValue;
 
   location.assign(queryString);
+}
+
+//save button
+saveBtn.addEventListener("click", function(event)
+{
+event.preventDefault();
+if (currentCity == "") {
+   return;
+}
+storeSave();
+displaySave();
 })
+
+//push search input into save list in local storage
+function storeSave() {
+saveList.push(currentCity);
+localStorage.setItem("saved", JSON.stringify(saveList));
+}
+
+//displays the saved search locations
+function displaySave() {
+  searchHistory.innerHTML = "";
+  for (var i = 0; i < saveList.length; i++) {
+    var save = saveList[i];
+
+    var li = document.createElement("li");
+    li.textContent = "";
+    li.setAttribute("saveSlot", i);
+
+    var button = document.createElement("button");
+    button.textContent = save;
+
+    li.appendChild(button);
+    searchHistory.appendChild(li);
+  }
+}
+
+//button to re-search saved city
+searchHistory.addEventListener("click", function(event) {
+  var element = event.target;
+
+  if (element.matches("button") === true) {
+    var index = element.parentElement.getAttribute("saveSlot");
+   console.log(index);
+   inputValue = saveList[index];
+   startSearch();
+
+  }
+});
+
+//generate initial input from url
 function getInput(){
   inputValue = document.location.search.split('=')[1];
   console.log(inputValue);
 }
 getInput();
 
-
+//generate today's weather
 function generateWeather() {
   var searchAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + inputValue + "&appid=" + APIkey + "&units=metric";
   console.log(inputValue);
@@ -63,7 +134,7 @@ function generateWeather() {
       console.log(data.name);
       console.log(data.main.feels_like);
       console.log(dayjs.unix(data.dt).format("MM/DD/YYYY"));
-
+      currentCity = data.name
       //display results to today
       var tempC = data.main.temp +String.fromCharCode(176) +"C";
       var windms = data.wind.speed + "m/s";
@@ -71,10 +142,9 @@ function generateWeather() {
       var day0icon = "<span class=\"day0Icon\"><img src=\"./assets/icons/" + data.weather[0].icon + ".png\"/></span>";
       day0.innerHTML = data.name + " (" + dayjs.unix(data.dt).format("MM/DD/YYYY") + ")" + day0icon;
       console.log(dayjs.unix(data.dt).format("MM/DD/YYYY"));
-      day0temp.textContent = "Temprature: " + tempC
-      day0wind.textContent = "Wind Speed: " + windms
-      day0humidity.textContent = "Humidity: " + humid
-
+      day0temp.textContent = "Temprature: " + tempC;
+      day0wind.textContent = "Wind Speed: " + windms;
+      day0humidity.textContent = "Humidity: " + humid;
   
       })
     .catch(function (error) {
@@ -86,81 +156,12 @@ function generateWeather() {
 }
 generateWeather()
 
-
-//sample copy from week6 01-27 script.js
-
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-
-//sample copy from week6 01-27 display-search.js
-var resultTextEl = document.querySelector('#result-text');
-var resultContentEl = document.querySelector('#result-content');
-var searchFormEl = document.querySelector('#search-form');
-
-function getParams() {
-  // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
-  var searchParamsArr = document.location.search.split('&');
-
-  // Get the query and format values
-  var query = searchParamsArr[0].split('=').pop();
-  var format = searchParamsArr[1].split('=').pop();
-
-  searchApi(query, format);
-}
-
-function printResults(resultObj) {
-  console.log(resultObj);
-
-  // set up `<div>` to hold result content
-  var resultCard = document.createElement('div');
-  resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
-
-  var resultBody = document.createElement('div');
-  resultBody.classList.add('card-body');
-  resultCard.append(resultBody);
-
-  var titleEl = document.createElement('h3');
-  titleEl.textContent = resultObj.title;
-
-  var bodyContentEl = document.createElement('p');
-  bodyContentEl.innerHTML =
-    '<strong>Date:</strong> ' + resultObj.date + '<br/>';
-
-  if (resultObj.subject) {
-    bodyContentEl.innerHTML +=
-      '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
-  } else {
-    bodyContentEl.innerHTML +=
-      '<strong>Subjects:</strong> No subject for this entry.';
-  }
-
-  if (resultObj.description) {
-    bodyContentEl.innerHTML +=
-      '<strong>Description:</strong> ' + resultObj.description[0];
-  } else {
-    bodyContentEl.innerHTML +=
-      '<strong>Description:</strong>  No description for this entry.';
-  }
-
-  var linkButtonEl = document.createElement('a');
-  linkButtonEl.textContent = 'Read More';
-  linkButtonEl.setAttribute('href', resultObj.url);
-  linkButtonEl.classList.add('btn', 'btn-dark');
-
-  resultBody.append(titleEl, bodyContentEl, linkButtonEl);
-
-  resultContentEl.append(resultCard);
-}
-
-function searchApi(query, format) {
-  var locQueryUrl = 'https://www.loc.gov/search/?fo=json';
-
-  if (format) {
-    locQueryUrl = 'https://www.loc.gov/' + format + '/?fo=json';
-  }
-
-  locQueryUrl = locQueryUrl + '&q=' + query;
-
-  fetch(locQueryUrl)
+//generate 5 day forecast
+function generateForecast() {
+  var searchAPI = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputValue + "&appid=" + APIkey + "&units=metric&cnt=36";
+  console.log(inputValue);
+  console.log(searchAPI);
+  fetch(searchAPI)
     .then(function (response) {
       if (!response.ok) {
         throw response.json();
@@ -168,42 +169,106 @@ function searchApi(query, format) {
 
       return response.json();
     })
-    .then(function (locRes) {
-      // write query to page so user knows what they are viewing
-      resultTextEl.textContent = locRes.search.query;
+    .then(function (data) {
+      console.log(data);
+      console.log(data.list);
+      console.log(data.list[1]);
+      console.log(dayjs.unix(data.list[3].dt).format("MM/DD/YYYY - h:mm A"));
+      console.log(dayjs.unix(data.list[11].dt).format("MM/DD/YYYY - h:mm A"));
+      console.log(dayjs.unix(data.list[19].dt).format("MM/DD/YYYY - h:mm A"));
+      console.log(dayjs.unix(data.list[27].dt).format("MM/DD/YYYY - h:mm A"));
+      console.log(dayjs.unix(data.list[35].dt).format("MM/DD/YYYY - h:mm A"));
 
-      console.log(locRes);
+      // for (var i = 3; i < data.list.length; i + 8) {
+      //   var dayData = data.list[i]
+      //   console.log(dayjs.unix(dayData.dt).format("MM/DD/YYYY - h:mm A"));
+      //   function loadForecast(i){}}
 
-      if (!locRes.results.length) {
-        console.log('No results found!');
-        resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-      } else {
-        resultContentEl.textContent = '';
-        for (var i = 0; i < locRes.results.length; i++) {
-          printResults(locRes.results[i]);
-        }
+      //display forecast day1
+      function generateDay1(data){
+      data = data.list[3];
+        var daytempC = data.main.temp +String.fromCharCode(176) +"C";
+        var daywindms = data.wind.speed + "m/s";
+        var dayhumid = data.main.humidity + "%";
+        var dayicon = "<span class=\"day3icon\"><img src=\"./assets/icons/" + data.weather[0].icon + ".png\"/></span>";
+        day3.textContent = dayjs.unix(data.dt).format("MM/DD/YYYY");
+        day3.innerHTML = dayjs.unix(data.dt).format("MM/DD/YYYY") + dayicon;
+        day3temp.textContent = "Temp: " + daytempC
+        day3wind.textContent = "Wind: " + daywindms
+        day3humidity.textContent = "Humidity: " + dayhumid
+        return;
       }
-    })
+      generateDay1(data)
+
+      //display forecast day2
+      function generateDay2(data){
+      data = data.list[11];
+        var daytempC = data.main.temp +String.fromCharCode(176) +"C";
+        var daywindms = data.wind.speed + "m/s";
+        var dayhumid = data.main.humidity + "%";
+        var dayicon = "<span class=\"day3icon\"><img src=\"./assets/icons/" + data.weather[0].icon + ".png\"/></span>";
+        day11.textContent = dayjs.unix(data.dt).format("MM/DD/YYYY");
+        day11.innerHTML = dayjs.unix(data.dt).format("MM/DD/YYYY") + dayicon;
+        day11temp.textContent = "Temp: " + daytempC
+        day11wind.textContent = "Wind: " + daywindms
+        day11humidity.textContent = "Humidity: " + dayhumid
+        return;
+      }
+      generateDay2(data)
+
+      //display forecast day3
+      function generateDay3(data){
+        data = data.list[19];
+          var daytempC = data.main.temp +String.fromCharCode(176) +"C";
+          var daywindms = data.wind.speed + "m/s";
+          var dayhumid = data.main.humidity + "%";
+          var dayicon = "<span class=\"day3icon\"><img src=\"./assets/icons/" + data.weather[0].icon + ".png\"/></span>";
+          day19.textContent = dayjs.unix(data.dt).format("MM/DD/YYYY");
+          day19.innerHTML = dayjs.unix(data.dt).format("MM/DD/YYYY") + dayicon;
+          day19temp.textContent = "Temp: " + daytempC
+          day19wind.textContent = "Wind: " + daywindms
+          day19humidity.textContent = "Humidity: " + dayhumid
+          return;
+        }
+        generateDay3(data)
+
+      //display forecast day4
+      function generateDay4(data){
+        data = data.list[27];
+          var daytempC = data.main.temp +String.fromCharCode(176) +"C";
+          var daywindms = data.wind.speed + "m/s";
+          var dayhumid = data.main.humidity + "%";
+          var dayicon = "<span class=\"day3icon\"><img src=\"./assets/icons/" + data.weather[0].icon + ".png\"/></span>";
+          day27.textContent = dayjs.unix(data.dt).format("MM/DD/YYYY");
+          day27.innerHTML = dayjs.unix(data.dt).format("MM/DD/YYYY") + dayicon;
+          day27temp.textContent = "Temp: " + daytempC
+          day27wind.textContent = "Wind: " + daywindms
+          day27humidity.textContent = "Humidity: " + dayhumid
+          return;
+        }
+        generateDay4(data)
+
+      //display forecast day3
+      function generateDay5(data){
+        data = data.list[35];
+          var daytempC = data.main.temp +String.fromCharCode(176) +"C";
+          var daywindms = data.wind.speed + "m/s";
+          var dayhumid = data.main.humidity + "%";
+          var dayicon = "<span class=\"day3icon\"><img src=\"./assets/icons/" + data.weather[0].icon + ".png\"/></span>";
+          day35.textContent = dayjs.unix(data.dt).format("MM/DD/YYYY");
+          day35.innerHTML = dayjs.unix(data.dt).format("MM/DD/YYYY") + dayicon;
+          day35temp.textContent = "Temp: " + daytempC
+          day35wind.textContent = "Wind: " + daywindms
+          day35humidity.textContent = "Humidity: " + dayhumid
+          return;
+        }
+        generateDay5(data)
+      })
     .catch(function (error) {
       console.error(error);
+      console.log("City name not recognized");
     });
-}
-
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
-
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
-
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
     return;
-  }
-
-  searchApi(searchInputVal, formatInputVal);
 }
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-
-getParams();
-
+generateForecast()
